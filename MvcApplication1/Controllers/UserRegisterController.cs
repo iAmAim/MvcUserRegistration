689440f39f6a18eaa0efb5b1a6420f02d9891884
iAmAim <schnider_17@yahoo.com>
2013-11-26 17:00:05 +0000

    working user registration form
    working user registration form

---
+++ MvcApplication1/Controllers/UserRegisterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistrationApplication.Models;
+
+namespace UserRegistrationApplication.Controllers
+{
+    public class UserRegisterController : Controller
+    {
+        //
+        // GET: /UserRegister/
+
+        public ActionResult UserRegister()
+        {
+            UserRegisterModel model = new UserRegisterModel();
+            return View(model);
+        }
+
+    }
+}
