7f76a9a33511b11789099065673d934c433848cc
iAmAim <schnider_17@yahoo.com>
2013-11-27 17:48:07 +0000

    added custom parameter and validation
    - added validation

--- MvcApplication1/Controllers/UserRegisterController.cs
+++ MvcApplication1/Controllers/UserRegisterController.cs
@@ -9,14 +9,34 @@ namespace UserRegistrationApplication.Controllers
 {
     public class UserRegisterController : Controller
     {
-        //
-        // GET: /UserRegister/
 
+
+        // GET: /UserRegister/
         public ActionResult UserRegister()
         {
             UserRegisterModel model = new UserRegisterModel();
             return View(model);
         }
+
+        [HttpPost]
+        public ActionResult UserRegister(UserRegisterModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                System.Text.StringBuilder sbMessage = new System.Text.StringBuilder();
+                sbMessage.Append("Your Name is : " + model.Name + "</br/>");
+                sbMessage.Append("Your Password is : " + model.Password + "</br/>");
+                sbMessage.Append("Your Email is : " + model.Email + "</br/>");
+                return Content(sbMessage.ToString());
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+
 
     }
 }
--- MvcApplication1/Models/UserRegisterModel.cs
+++ MvcApplication1/Models/UserRegisterModel.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace UserRegistrationApplication.Models
 {
     public class UserRegisterModel
     {
+        [DisplayName("User Name")]
+        [Required(ErrorMessage = "Name can not be blank")]
         public string Name {get; set;}
         public string Password { get; set; }
         public string Email { get; set; }
